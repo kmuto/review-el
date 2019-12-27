1c72334989fe01f2a69b23d2aebea39fa5e099df
Kenshi Muto <kmuto@debian.org>
2019-12-27 10:08:57 +0900

    単一ブロック命令を分離

--- tags/sort.rb
+++ tags/sort.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/env ruby
-ary = []
+ary1 = []
+ary2 = []
 ARGF.each do |l|
-  ary.push(l.chomp)
+  op, opt = l.chomp.split("\t")
+  if opt =~ /s/ # single
+    ary2.push(op)
+  else
+    ary1.push(op)
+  end
 end
-puts ary.sort.map {|i| %Q("#{i}") }.join(' ')
+puts ary1.sort.map {|i| %Q("#{i}") }.join(' ')
+puts ary2.sort.map {|i| %Q("#{i}") }.join(' ') unless ary2.empty?
